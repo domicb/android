f553c4d86d9ccb3f9f18ff6212496715732f1b72
domicb <domi1213@hotmail.com>
2016-03-10 18:57:40 +0100

    Revert "algunos cambios"
    This reverts commit 958714c3283afb7fbb2a8efd5606ead5229c88e4.

--- AndroidLibros/app/src/main/java/com/example/domingo/androidlibros/VistaLibro.java
+++ AndroidLibros/app/src/main/java/com/example/domingo/androidlibros/VistaLibro.java
@@ -236,7 +236,7 @@ public class VistaLibro extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
-                DB.insert(
+                DB.insertLibro(
                         et_titulo.getText().toString(),
                         et_autor.getText().toString(),
                         et_editorial.getText().toString(),
